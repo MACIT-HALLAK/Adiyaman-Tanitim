ac6c91808cd96a45121c1b95f5cd10f113380645
ABDULLAH GHAZAL <omaargh001@gmail.com>
2023-09-11 13:54:09 +0300

    navbar editing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import './App.css';
 import ImageGallery from './Components/Slider/ImageGallery/ImageGallery';
-import Slider from './Components/Slider/Slider';
 import Navbar from './Components/Navbar/Navbar';
 import Content from './Components/Content/Content';
-import TitleCom from './Components/Slider/TitleCom/TitleCom'
-import Statistics from './Components/Statistics/Statistics'
-import Footer from './Components/Footer/Footer'
-
+import TitleCom from './Components/Slider/TitleCom/TitleCom';
+import Statistics from './Components/Statistics/Statistics';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar/>
-        <Content/>
-        <Statistics/>
+        <Navbar />
+        <Content />
+        <Statistics />
         <TitleCom />
         <ImageGallery />
-        <Footer/>
+        <Footer />
       </header>
     </div>
   );
