9e8aa46eed1061bafe1f5d511fd160bb79240572
ABDULLAH GHAZAL <omaargh001@gmail.com>
2023-09-18 01:33:14 +0300

    some edits

--- src/Components/Areas/Areas.js
+++ src/Components/Areas/Areas.js
@@ -5,25 +5,22 @@ import image1 from '../../Assets/images/ww.jpg';
 import image2 from '../../Assets/images/www.JPG';
 import image3 from '../../Assets/images/wwwww.webp';
 const Areas = () => {
-
-
-
   return (
     <div className="Areas-layout">
       <Navber />
       <div className="main-areas">
         <h2>Adiyamanda gezilecek Alanlar</h2>
         <ul className="Areas-navigation">
-          <li className='areas-li' >
+          <li className="areas-li">
             <a href="#Cendere">Cendere Köprüsü</a>
           </li>
-          <li className='areas-li'>
+          <li className="areas-li">
             <a href="#Perre">Perre Antik Kenti</a>
           </li>
-          <li className='areas-li'>
+          <li className="areas-li">
             <a href="#Eski">Eski Besni Harabeleri</a>
           </li>
-          <li className='areas-li'>
+          <li className="areas-li">
             <a href="#yuzen">Yüzen Adalar</a>
           </li>
         </ul>
@@ -35,9 +32,11 @@ const Areas = () => {
             <div>
               <h2>Cendere Köprüsü</h2>
               <p>
-                  Roma hükümdarı Septimus Severus tarafından inşaa ettirilen yapı Anadolu’nun en eski köprülerinden biridir.
-                  Köprünün en önemli özelliklerinden biri 120 metre uzunluğundaki köprüde hiç çimento kullanılmamış olması.
-                  Cendere Çayı üzerine yapılan köprü, Adıyaman’ın Kahta ile Sincik ilçelerini birbirine bağlıyor.
+                Roma hükümdarı Septimus Severus tarafından inşaa ettirilen yapı
+                Anadolu’nun en eski köprülerinden biridir. Köprünün en önemli
+                özelliklerinden biri 120 metre uzunluğundaki köprüde hiç çimento
+                kullanılmamış olması. Cendere Çayı üzerine yapılan köprü,
+                Adıyaman’ın Kahta ile Sincik ilçelerini birbirine bağlıyor.
               </p>
             </div>
           </div>
@@ -48,9 +47,14 @@ const Areas = () => {
             <div>
               <h2>Perre Antik Kenti</h2>
               <p>
-                  Kommagene İmparatorluğu’nun bıraktığı yazıtlarda bahsedilen antik kentlerden biri olan Perre zengin su kaynaklarına sahip olmasıyla ün kazanmış. Şehrin kale duvarlarının birçoğu da hala mevcut.
-                  Roma İmparatorluğu’nun bu şehre kattığı Roma Çeşmesi de dönemin durak yerlerinden biri olan şehre dağları aşarak yolculuk ulaşmaya çalışanlar için mükemmel bir imkan yaratmış.
-                  Antik kente, şehir merkezine sadece 4 km uzaklıkta olmasıyla çok kolay bir şekilde ulaşılabilir.
+                Kommagene İmparatorluğu’nun bıraktığı yazıtlarda bahsedilen
+                antik kentlerden biri olan Perre zengin su kaynaklarına sahip
+                olmasıyla ün kazanmış. Şehrin kale duvarlarının birçoğu da hala
+                mevcut. Roma İmparatorluğu’nun bu şehre kattığı Roma Çeşmesi de
+                dönemin durak yerlerinden biri olan şehre dağları aşarak
+                yolculuk ulaşmaya çalışanlar için mükemmel bir imkan yaratmış.
+                Antik kente, şehir merkezine sadece 4 km uzaklıkta olmasıyla çok
+                kolay bir şekilde ulaşılabilir.
               </p>
             </div>
           </div>
@@ -61,8 +65,15 @@ const Areas = () => {
             <div>
               <h2>Eski Besni Harabeleri</h2>
               <p>
-                  Besni, yüzyıllar boyunca birçok medeniyete ev sahipliği yapmış ve sanayi ve ticaret kenti olarak da tanınmıştır. Besni Kalesi’yle de beraber şehir korunaklı kalmış; sulak ve ormanlık arazi varlığıyla zengin bir yerleşim yeri olarak görülmüştür.
-                  Besni ilçesinin 4-5 km güneyinde bulunan Eski Besni Ören Yeri, Besni Kalesi’ni bütünleyen Ulu Camii, Külhanönü Camii, Tokatmış Camii gibi cami ve minareler, Bekir Bey Külliyesi’nden kalan hamam ve çeşme ile diğer külliye, köprü ve çarşı kalıntılarıyla oldukça çekici bir görünüme sahiptir.
+                Besni, yüzyıllar boyunca birçok medeniyete ev sahipliği yapmış
+                ve sanayi ve ticaret kenti olarak da tanınmıştır. Besni
+                Kalesi’yle de beraber şehir korunaklı kalmış; sulak ve ormanlık
+                arazi varlığıyla zengin bir yerleşim yeri olarak görülmüştür.
+                Besni ilçesinin 4-5 km güneyinde bulunan Eski Besni Ören Yeri,
+                Besni Kalesi’ni bütünleyen Ulu Camii, Külhanönü Camii, Tokatmış
+                Camii gibi cami ve minareler, Bekir Bey Külliyesi’nden kalan
+                hamam ve çeşme ile diğer külliye, köprü ve çarşı kalıntılarıyla
+                oldukça çekici bir görünüme sahiptir.
               </p>
             </div>
           </div>
@@ -73,8 +84,11 @@ const Areas = () => {
             <div>
               <h2>Yüzen Adalar</h2>
               <p>
-                  Adıyaman’ın Çelikhan ilçesinde bulunan Çat Barajı’nda farklı büyüklüklerde onlarca yüzen ada bulunuyor.
-                  Sürekli şekil, boyut ve yer değiştiren bu küçük adacıkların binlerce yılda oluştuğu düşünülüyor. Adacıkların bir kısmı kıyaya vurarak artık hareket edemez oluyor.
+                Adıyaman’ın Çelikhan ilçesinde bulunan Çat Barajı’nda farklı
+                büyüklüklerde onlarca yüzen ada bulunuyor. Sürekli şekil, boyut
+                ve yer değiştiren bu küçük adacıkların binlerce yılda oluştuğu
+                düşünülüyor. Adacıkların bir kısmı kıyaya vurarak artık hareket
+                edemez oluyor.
               </p>
             </div>
           </div>
