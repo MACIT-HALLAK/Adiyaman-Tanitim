6def8b4af3c85d383a6c59b134966b2da44248db
ABDULLAH GHAZAL <omaargh001@gmail.com>
2023-10-06 17:26:37 +0300

    image editing

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,24 +1,24 @@
 import './Content.css';
 import React, { useState, useEffect, useRef, Suspense } from 'react';
 
-import slidimage1 from '../../Assets/images/slider1.jpeg';
-import slidimage2 from '../../Assets/images/slider2.jpeg';
-import slidimage3 from '../../Assets/images/slider3.jpeg';
-import slidimage4 from '../../Assets/images/slider4.jpeg';
-import slidimage5 from '../../Assets/images/slider5.jpeg';
-import slidimage6 from '../../Assets/images/slider6.jpeg';
-import slidimage7 from '../../Assets/images/slider7.jpeg';
-import slidimage8 from '../../Assets/images/slider8.jpeg';
-import slidimage9 from '../../Assets/images/slider9.jpeg';
-import slidimage10 from '../../Assets/images/slider10.jpeg';
-import slidimage11 from '../../Assets/images/slider11.jpeg';
-import slidimage12 from '../../Assets/images/slider12.jpeg';
-import slidimage13 from '../../Assets/images/slider13.jpeg';
-import slidimage14 from '../../Assets/images/slider14.jpeg';
-import slidimage15 from '../../Assets/images/slider15.jpeg';
-import slidimage16 from '../../Assets/images/slider16.jpeg';
-import slidimage17 from '../../Assets/images/slider17.jpeg';
-import slidimage18 from '../../Assets/images/slider18.jpeg';
+import slidimage1 from '../../Assets/images/sliders/slider1.jpeg';
+import slidimage2 from '../../Assets/images/sliders/slider2.jpeg';
+import slidimage3 from '../../Assets/images/sliders/slider3.jpeg';
+import slidimage4 from '../../Assets/images/sliders/slider4.jpeg';
+import slidimage5 from '../../Assets/images/sliders/slider5.jpeg';
+import slidimage6 from '../../Assets/images/sliders/slider6.jpeg';
+import slidimage7 from '../../Assets/images/sliders/slider7.jpeg';
+import slidimage8 from '../../Assets/images/sliders/slider8.jpeg';
+import slidimage9 from '../../Assets/images/sliders/slider9.jpeg';
+import slidimage10 from '../../Assets/images/sliders/slider10.jpeg';
+import slidimage11 from '../../Assets/images/sliders/slider11.jpeg';
+import slidimage12 from '../../Assets/images/sliders/slider12.jpeg';
+import slidimage13 from '../../Assets/images/sliders/slider13.jpeg';
+import slidimage14 from '../../Assets/images/sliders/slider14.jpeg';
+import slidimage15 from '../../Assets/images/sliders/slider15.jpeg';
+import slidimage16 from '../../Assets/images/sliders/slider16.jpeg';
+import slidimage17 from '../../Assets/images/sliders/slider17.jpeg';
+import slidimage18 from '../../Assets/images/sliders/slider18.jpeg';
 import { Link } from 'react-router-dom';
 
 const ImageComponent = React.lazy(() => import('../skeletons/ImageComponent'));
@@ -66,7 +66,7 @@ const Content = () => {
   }, []);
 
   const [text, setText] = useState('');
-  const sentence = 'AADIYAMAN ŞEHRINE HOŞ GELDINIZ';
+  const sentence = 'ADIYAMAN’I KEŞFETMEYE HAZIR MISINIZ?';
   const delay = 100;
 
   useEffect(() => {
@@ -92,6 +92,98 @@ const Content = () => {
 
     return () => clearInterval(interval);
   }, []);
+  const [textValues, setTextValues] = useState([
+    {
+      title: 'Abuzer Gaffari Türbesi',
+      content:
+        'Sahabelerden olduğu söylenir. Adıyaman’da savaşırken düşmanlar tarafından şehit edildiği ve buraya gömüldüğü rivayet edilmektedir.',
+    },
+    {
+      title: 'Adıyaman Kalesi',
+      content:
+        'Adıyaman şehir merkezinde yer alan kale yaklaşık 25 metre yükseklikteki yığma bir höyük üzerine kurulmuştur. VII. yüzyıl ortalarında Bizans saldırılarına karşı koymak amacıyla Emevi komutanı Mansur İbn-i Cavana yaptırdığı bilinmektedir',
+    },
+    {
+      title: 'Altınlı Köprüsü',
+      content:
+        'Adıyaman’ın Gölbaşı ilçesine bağlı olan Yolbağ Köyü’ndeki Altınlı Köprü, şehrin en eski kalıntılarının başında geliyor. Altınlı Köprü’nün tarihi Roma dönemlerine dayanmakta ve günümüze kadar çok fazla tadilat görmemiştir. Yapımı sırasında beyaz kesme taş ve moloz taş kullanıldığı görülen köprü, büyük dört kemerden oluşmaktadır. Yapım tarihi ise kesin olarak bilinmemektedir',
+    },
+    {
+      title: 'Arsemia',
+      content:
+        'Arsemia Antik Kenti, Nemrut güzergâhı üzerindeki bir başka ören yeri. Kommagene medeniyetinin bir dönem başkentiymiş burası. Kale olarak da adlandırılan Arsemia, savunma amacıyla kullanılmasının yanısıra dini amaçlı tüneller kazmak için de kullanılmış',
+    },
+    {
+      title: 'Nemrud Arslanlı Horoskop',
+      content:
+        "Aslanın gövdesinde 19 yıldız vardır. Her yıldız sivri uçlu sekiz ışından oluşur. Konumlarındaki küçük değişiklikler dışında, bu yıldızlar Eratostenes'in Ephemeris'inde tasvir ettiği Aslan Konstelasyonu'nu temsil eder.",
+    },
+    {
+      title: 'Cendere Köprüsü',
+      content:
+        'Cendere Köprüsü, Roma İmparatoru Septimius Severus adına M.S. 193 yılında inşa edilmiş olup halen kullanılmakta olan en eski köprülerden birisidir.',
+    },
+    {
+      title: 'Dikilitaş (Sesönk)',
+      content:
+        'Besni ilçesi Hacıhalil ve Dikilitaş köyü yakınlarında konumlanan, Roma dönemine ait anıt mezar olan Dikilitaş (Sesönk), Kızıldağ üzerinde yer alır.',
+    },
+    {
+      title: 'Gerger Kalesi',
+      content:
+        "Gerger Kalesi Adıyaman Oymaklı Köyü'nde bulunmaktadır. Tarihi Geç Hitit Beylikleri Dönemi'ne kadar dayanır. Ancak Eski Gerger Kenti'nin MÖ. 3'üncü yüzyılda Arsames adında Seleukos kralı zamanında kurulduğu bilinmektedir. MÖ. II. yüzyılda ise Fırat (Euphrat) Arsameiası olarak adlandırılmaktaydı.",
+    },
+    {
+      title: 'Gerger Kanyonu',
+      content:
+        '2004 yılında kent turizmine kazandırılan Gerger Kanyonu, Fırat Nehri ile Atatürk Baraj Gölü’nün kesiştiği noktada yer almaktadır. Eskiden çok fazla bilinmeyen bu kanyon, son yıllarda popüler olmaya başlamıştır. Yolunuz buraya düşerse bu doğal güzelliğin tadını çıkarmak için mutlaka Gerger Kanyonu’na uğramalısınız.',
+    },
+    {
+      title: 'Antiochos Apollon',
+      content:
+        "Adıyaman'ın Gerger ilçesinde keşfedilen Temenosta Kral l. Antiochos ve Tanrı Apollon'un tokalaşma sahnesi ile Antiochos kanunların yazılı olduğu steller görülmeye değer.",
+    },
+    {
+      title: 'Karakuş Tümülüsü',
+      content:
+        'Adıyaman-Kahta girişinde bulunan, Kommagene Kralı II. Mithridates tarafından annesi İsas adına yaptırılan anıt mezar, sütun üzerindeki kartaldan dolayı Karakuş Tümülüsü olarak anılmaktadır.',
+    },
+    {
+      title: 'Keleş Konağı',
+      content:
+        'Keleş Konağı, Adıyaman Merkez ilçesinde Hısn-ı Mansur Kalesi’nin altında bulunuyor. Buranın restorasyonu tamamlanmıştır. Kale yolu ve merpenlerinin yenilendiği konak günümüzde sosyal tesis olarak hizmet veriyor.',
+    },
+    {
+      title: 'Mahmud El Ensari',
+      content:
+        'Medineli bir Sahabe olduğu rivayet edilmektedir. İslamiyet’i yaymak amacıyla Anadolu’ya gelmiş ve burada şehit düşmüştür. Mezarının bulunduğu yerde başka şehitlerin de olduğu rivayet edilir.',
+    },
+    {
+      title: 'Musalla Cami',
+      content:
+        'Adıyaman merkezde bulunan camiinin yapım tarihi kesin olarak bilinmemektedir. 1308 hicri 1890 miladi yılında deprem sonucu minaresinin yıkıldığı rivayet edilmektedir. Dolayısıyla daha önce inşa edilmiş olmalıdır.',
+    },
+    {
+      title: 'Nemrut Dağı Örenyeri',
+      content:
+        ' 1987 yılında UNESCO İnsanlığın Kültür Mirası listesine girmiş olan Nemrut Dağı Örenyeri, İl merkezine 77 km, Kahta ilçesine 43 km uzaklıktadır. Dünya Miras Alanı bu yer, Doğu Toros sıradağları üzerinde 2.206 metre yükseklikte, Fırat Nehri geçitlerine ve ovaya hakim bir tepe üzerinde yer almaktadır.',
+    },
+    {
+      title: 'Perre Antik Kenti',
+      content:
+        'Eski kaynaklarda Me’arath gazze Pörön, Mezopotamya’da ise Pirin ve Perin olarak bilinen Kommagene Krallığı’nın beş büyük kentinden biri olan Perre Antik Kenti, Melitene’yi (Malatya) başkent Samosata’ya (Samsat) bağlayan yol güzergahı üzerinde olması nedeniyle geçmişte jeopolitik bir öneme sahipti.',
+    },
+    {
+      title: 'Mor Petrus ve Mor Paulus Kilisesi',
+      content:
+        "Mor Petrus ve Mor Paulus Kilisesi Adıyaman Merkez Mara Mahallesi'ndedir. 4 veya 5'inci yüzyılda yapıldığı tahmin edilen kilise, giriş kapısı üzerinde ve içeride bulunan Süryanice kitabelere göre 1888 ve 1905 yıllarında onarım görmüştür.",
+    },
+    {
+      title: 'Tuzhanı',
+      content:
+        'Tarihte yolcuların konaklama ve ticaret merkezi olarak inşa edilen ve günümüze kadar ayakta kalmayı başaran 200 yıllık Tarihi Tuz Hanı, aslına ve günümüz şartlarına uygun yapılan restorasyon çalışmalarının ardından mimari güzelliğiyle dikkatleri üzerinde topluyor.',
+    },
+  ]);
 
   return (
     <>
@@ -103,11 +195,8 @@ const Content = () => {
               _
             </span>
           </h2>
-          <p className="desc">
-            Adıyaman'daki tarihi ve arkeolojik alanları tanıtmak için
-            oluşturulmuş bir platformdur. Bu web sitesi, bölgedeki tarihi
-            zenginlikleri ve kültürel mirası ziyaretçilere tanıtmayı amaçlar.
-          </p>
+          <p>{textValues[currentImageIndex]?.title}</p>
+          <p className="desc">{textValues[currentImageIndex]?.content}</p>
           <button className="content-btn">
             <Link to={'/Videos'}>Şimdi Keşfet</Link>
           </button>
