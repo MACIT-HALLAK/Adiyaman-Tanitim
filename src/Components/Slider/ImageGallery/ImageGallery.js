4144a94018d25cc8c3688a4a72d625bb5688be9a
MACIT-HALLAK <hlaqm8169@gmail.com>
2023-10-07 12:57:56 -0900

    EDIT GALLERY COMPONENT

--- src/Components/Slider/ImageGallery/ImageGallery.js
+++ src/Components/Slider/ImageGallery/ImageGallery.js
@@ -1,47 +1,71 @@
-import './ImageGallery.css';
-import { data } from '../../../lib/dummy';
-import { useState } from 'react';
+import "./ImageGallery.css";
+import { data } from "../../../lib/dummy";
+import { useState } from "react";
 
 const ImageGallery = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [ImagCat, setImagCat] = useState("all");
 
   const handleClick = (image) => {
     setSelectedImage(image);
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
   };
   const handleClickClose = () => {
     setSelectedImage(null);
-    document.body.style.overflow = 'scroll';
+    document.body.style.overflow = "scroll";
+  };
+  const handleImagCat = (val) => {
+    setImagCat(val);
   };
 
   return (
     <div className="filters-com">
       <div className="filters-wraper">
         <div>
-          <div>All</div>
-          <div>Camiler</div>
-          <div>Turism mekanlari</div>
-          <div>Yemekleri</div>
-          <div>home</div>
+          <div  onClick={() => handleImagCat("all")}>All</div>
+          <div className="activ" onClick={() => handleImagCat("cami")}>Camiler</div>
+          <div onClick={() => handleImagCat("tursim")}>Turism mekanlari</div>
+          <div onClick={() => handleImagCat("proje")}>Projeler</div>
+          <div onClick={() => handleImagCat("yemek")}>Yemekleri</div>
         </div>
       </div>
       <div className="gallery-wraper">
         {data?.map((item, index) => {
-          return (
-            <div key={index}>
-              <div className="image-gallery">
-                <img
-                  onClick={() => handleClick(item.img)}
-                  src={item.img}
-                  alt={`${index}`}
-                />
-                <div className="text">
-                  <h2 className="title">{item.title}</h2>
-                  <p className="desc">{item.desc}</p>
+          if(ImagCat === "all")
+          {
+            return (
+              <div key={index}>
+                <div className="image-gallery">
+                  <img
+                    onClick={() => handleClick(item.img)}
+                    src={item.img}
+                    alt={`${index}`}
+                  />
+                  <div className="text">
+                    <h2 className="title">{item.title}</h2>
+                    <p className="desc">{item.desc}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
+            );
+          }
+          if (item.cat === ImagCat) {
+            return (
+              <div key={index}>
+                <div className="image-gallery">
+                  <img
+                    onClick={() => handleClick(item.img)}
+                    src={item.img}
+                    alt={`${index}`}
+                  />
+                  <div className="text">
+                    <h2 className="title">{item.title}</h2>
+                    <p className="desc">{item.desc}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          }
         })}
       </div>
       {selectedImage && (
--- src/lib/dummy.js
+++ src/lib/dummy.js
@@ -3,31 +3,113 @@ import slidimage2 from '../Assets/images/tt.jpg';
 import slidimage3 from '../Assets/images/ttt.jpg';
 import slidimage4 from '../Assets/images/tt.jpg';
 import slidimage5 from '../Assets/images/tttt.jpg';
+import galleryImg1 from '../Assets/images/camiler/ULI.JPG';
+import galleryImg2 from '../Assets/images/camiler/ulu.jpg';
+import galleryImg3 from '../Assets/images/camiler/altin.JPG';
+import galleryImg4 from '../Assets/images/camiler/CARSI.JPG';
+import galleryImg5 from '../Assets/images/camiler/kap.JPG';
+import galleryImg6 from '../Assets/images/camiler/yeni.JPG';
+import galleryImg7 from '../Assets/images/camiler/dursun.JPG';
+
+import galleryImg8 from '../Assets/images/tarih/t1.JPG';
+import galleryImg9 from '../Assets/images/tarih/t2.JPG';
+import galleryImg10 from '../Assets/images/tarih/t3.JPG';
+import galleryImg11 from '../Assets/images/tarih/t4.JPG';
+import galleryImg12 from '../Assets/images/tarih/t5.JPG';
+import galleryImg13 from '../Assets/images/tarih/t6.JPG';
+import galleryImg14 from '../Assets/images/tarih/t7.JPG';
+import galleryImg15 from '../Assets/images/tarih/t8.JPG';
 
 export const data = [
   {
-    img: slider1,
-    title: '   Nemrut Dağı Milli Parkı',
+    cat:'cami',
+    img: galleryImg1,
+    title: 'MUSALLA CAMII',
     desc: 'Antik Medeniyetlerin İzinde',
   },
   {
-    img: slidimage2,
-    title: '   Cendere Köprüsü',
+    cat:'cami',
+    img: galleryImg2,
+    title: 'ULU CAMII',
     desc: 'Antik Medeniyetlerin İzinde',
   },
   {
-    img: slidimage3,
-    title: '   Perre Antik Kenti',
-    desc: 'Adıyaman’daki Müzeler',
+    cat:'cami',
+    img: galleryImg3,
+    title: 'ALTINSEHIR CAMII',
+    desc: 'Antik Medeniyetlerin İzinde',
   },
   {
-    img: slidimage4,
-    title: '   Eski Besni Harabeleri',
-    desc: 'Adıyaman’daki Doğal Güzellikler',
+    cat:'cami',
+    img: galleryImg4,
+    title: 'CARSI CAMII',
+    desc: 'Antik Medeniyetlerin İzinde',
   },
   {
-    img: slidimage5,
-    title: '   Oturakçı Pazarı',
-    desc: 'Adıyaman’da Alışveriş',
+    cat:'cami',
+    img: galleryImg5,
+    title: 'KAP CAMII',
+    desc: 'Antik Medeniyetlerin İzinde',
   },
+  {
+    cat:'cami',
+    img: galleryImg6,
+    title: 'YENIPINAR CAMII',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'cami',
+    img: galleryImg7,
+    title: 'DURSUN CAVUS CAMII',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'tursim',
+    img: galleryImg8,
+    title: 'Besni Bekirbey Hamamı Ulucami',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'tursim',
+    img: galleryImg9,
+    title: 'Besni Dikilitaş Sesönk',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'tursim',
+    img: galleryImg10,
+    title: 'Eski Kahta Kalesi',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'tursim',
+    img: galleryImg11,
+    title: 'Eski Kahta Kalesi',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'tursim',
+    img: galleryImg12,
+    title: 'Eski Kahta Kalesi',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'tursim',
+    img: galleryImg13,
+    title: 'Nemrut Dağı',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'tursim',
+    img: galleryImg14,
+    title: 'Nemrut Dağı',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  {
+    cat:'tursim',
+    img: galleryImg15,
+    title: 'Perre Nekropol Alanı',
+    desc: 'Antik Medeniyetlerin İzinde',
+  },
+  
 ];
