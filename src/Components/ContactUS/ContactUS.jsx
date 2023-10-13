a222b907e0768d63391353377ce66e851fb50aef
Baraa963 <127866968+Baraa963@users.noreply.github.com>
2023-10-13 23:42:43 +0300

    contactUs with grid layout

--- src/Components/ContactUS/ContactUS.jsx
+++ src/Components/ContactUS/ContactUS.jsx
@@ -4,191 +4,140 @@ import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import img from '../../Assets/images/itm2.png';
 import img2 from '../../Assets/images/icons8-mail-50 (1).png';
-import { useTranslation } from 'react-i18next';
 
 const ContactUS = () => {
-  const { t } = useTranslation();
-  const [typedText, setTypedText] = useState('');
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const textToType = t('contact.title');
+  const [typingText, setTypingText] = useState('');
+  const [currentCharacterIndex, setCurrentCharacterIndex] = useState(0);
+
+  // Text to be typed
+  const textToType = 'Bize Ulaşın';
 
   useEffect(() => {
-    const delay = 150; // 0.5 seconds delay
-    if (currentIndex < textToType.length) {
-      const timer = setTimeout(() => {
-        setTypedText(textToType.slice(0, currentIndex + 1));
-        setCurrentIndex(currentIndex + 1);
-      }, delay);
-      return () => clearTimeout(timer);
+    if (currentCharacterIndex < textToType.length) {
+      const timeoutId = setTimeout(() => {
+        setTypingText(textToType.substring(0, currentCharacterIndex + 1));
+        setCurrentCharacterIndex(currentCharacterIndex + 1);
+      }, 150);
+
+      return () => clearTimeout(timeoutId);
     }
-  }, [currentIndex, textToType]);
+  }, [currentCharacterIndex, textToType]);
 
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     subject: '',
     message: '',
-  });
-  const [errors, setErrors] = useState({
+    });
+
+    const [errors, setErrors] = useState({
     name: '',
     email: '',
     subject: '',
     message: '',
-  });
+    });
 
-  useEffect(() => {
-    setTimeout(() => {
-      setErrors({
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-      });
-    }, 3000);
-  }, [errors]);
-  const validateEmail = (email) => {
+    const validateEmail = (email) => {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
-  };
+    };
 
-  const handleSubmit = (e) => {
-    // Reset errors
-    setErrors({ name: '', email: '', message: '', subject: '' });
-
-    // Validate Name
-    if (!formData.name.trim()) {
-      setErrors((prevErrors) => ({ ...prevErrors, name: 'Name is required' }));
-      e.preventDefault();
-      return;
-    }
-
-    // Validate Email
-    if (!validateEmail(formData.email)) {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        email: 'Invalid email format',
-      }));
-      e.preventDefault();
-      return;
-    }
-
-    // Validate Subject
-    if (!formData.subject.trim()) {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        message: 'subject is required',
-      }));
-      e.preventDefault();
-      return;
-    }
-    // Validate Message
-    if (!formData.message.trim()) {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        message: 'Message is required',
-      }));
-      e.preventDefault();
-      return;
-    }
-  };
-
+    const handleSubmit = (e) => {
+      // Reset errors
+      setErrors({ name: '', email: '', message: '' });
+  
+      // Validate Name
+      if (!formData.name.trim()) {
+        setErrors((prevErrors) => ({ ...prevErrors, name: 'Name is required' }));
+        e.preventDefault();
+        return;
+      }
+  
+      // Validate Email
+      if (!validateEmail(formData.email)) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          email: 'Invalid email format',
+        }));
+        e.preventDefault();
+        return;
+      }
+  
+      // Validate Message
+      if (!formData.subject.trim()) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          subject: 'subject is required',
+        }));
+        e.preventDefault();
+        return;
+      }
+      // Validate Message
+      if (!formData.message.trim()) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          message: 'Message is required',
+        }));
+        e.preventDefault();
+        return;
+      }
+    };
+    
   return (
     <>
       <Navbar />
-      <div className="Contact-Us">
-        <section>
-          <div className='contact-head'>
-            <p className="contact-p">
-              {typedText}
-            </p>
-          </div>
-          
-            <img className="contact-img2" src={img2} alt="contact_img" />
-            <img className="contact-img" src={img} alt="contact_img" />
-          <div class="container-contact">
-            <div class="wrap-contact">
-              <form
-                name="contact"
-                class="contact-form validate-form"
-                action={`mailto:bilgiislem@adiyaman.bel.tr?subject=${formData.subject}&body=Name: ${formData.name}%0DEmail: ${formData.email}%0D%0DMessage: ${formData.message}`}
-                method="post"
-                onSubmit={handleSubmit}
-              >
-                <div class="wrap-input validate-input">
-                  <input
-                    className="input"
-                    type="text"
-                    name="name"
-                    placeholder={t('contact.inputs.name')}
-                    value={formData.name}
-                    onChange={(e) =>
-                      setFormData({ ...formData, name: e.target.value })
-                    }
-                  />
-                </div>
-                {errors.name && <div className="error">{errors.name}</div>}
+      <div className='contact-us'>
+        <div className='contact-form'>
+          <form
+          action={`mailto:mahmmmoud144@gmail.com?subject=Contact Form Submission&body=Name: ${formData.name}%0DEmail: ${formData.email}%0D%0DMessage: ${formData.message}`}
+          method="post"
+          onSubmit={handleSubmit}
+          />
 
-                <div className="wrap-input validate-input">
-                  <input
-                    className="input"
-                    type="text"
-                    name="email"
-                    placeholder={t('contact.inputs.email')}
-                    value={formData.email}
-                    onChange={(e) =>
-                      setFormData({ ...formData, email: e.target.value })
-                    }
-                  />
-                </div>
-                {errors.email && <div className="error">{errors.email}</div>}
+          <p className='c-p'>{typingText}</p>
+          <img className='c-img' src={img2}/>
 
-                <div className="wrap-input validate-input">
-                  <input
-                    className="input"
-                    type="text"
-                    name="email"
-                    placeholder={t('contact.inputs.subject')}
-                    value={formData.subject}
-                    onChange={(e) =>
-                      setFormData({ ...formData, subject: e.target.value })
-                    }
-                  />
-                </div>
-                {errors.subject && (
-                  <div className="error">{errors.subject}</div>
-                )}
-                <div className="wrap-input validate-input">
-                  <textarea
-                    className="input"
-                    type="text"
-                    name="message"
-                    rows={15}
-                    cols={135}
-                    placeholder={t('contact.inputs.message')}
-                    value={formData.message}
-                    onChange={(e) =>
-                      setFormData({ ...formData, message: e.target.value })
-                    }
-                  ></textarea>
-                </div>
-                {errors.message && (
-                  <div className="error">{errors.message}</div>
-                )}
-
-                <div className="container-contact-form-btn">
-                  <button className="contact-form-btn" type="submit">
-                    <span>{t('contact.inputs.button')}</span>
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </section>
-
-        <div className="contact-footer">
-          <Footer />
+          <input 
+            className='c-input1' 
+            placeholder='Ad-Soyad'
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          />
+          {errors.name && <div>{errors.name}</div>}
+          <input 
+            className='c-input2' 
+            placeholder='E-mail'
+            value={formData.email}
+            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          />
+          {errors.email && <div>{errors.email}</div>}
+          <input 
+            className='c-input3' 
+            placeholder='Konu Başlığı'
+            value={formData.subject}
+            onChange={(e) => setFormData({ ...formData, subject: e.target.value })}
+          />
+          {errors.subject && <div>{errors.subject}</div>}
+          <textarea 
+            className='c-text' 
+            placeholder='Mesaj'
+            value={formData.message}
+            onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+          >
+          </textarea>
+          {errors.message && <div>{errors.message}</div>}
+          <input type="submit" value="Send" className='c-button'/>
+        <form/>
         </div>
+        <div className='contact-img'>
+          <img className='c-img1' src={img}/>
+          
+        </div>
+
       </div>
+
+      <Footer />
+  
     </>
   );
 };
