8483ec287ede8b2bb74722f874139fb3d8cc17b0
ABDULLAH GHAZAL <omaargh001@gmail.com>
2023-10-14 00:05:16 +0300

    some edits

--- src/Components/ContactUS/ContactUS.jsx
+++ src/Components/ContactUS/ContactUS.jsx
@@ -28,116 +28,121 @@ const ContactUS = () => {
     email: '',
     subject: '',
     message: '',
-    });
+  });
 
-    const [errors, setErrors] = useState({
+  const [errors, setErrors] = useState({
     name: '',
     email: '',
     subject: '',
     message: '',
-    });
+  });
 
-    const validateEmail = (email) => {
+  const validateEmail = (email) => {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
-    };
+  };
 
-    const handleSubmit = (e) => {
-      // Reset errors
-      setErrors({ name: '', email: '', message: '' });
-  
-      // Validate Name
-      if (!formData.name.trim()) {
-        setErrors((prevErrors) => ({ ...prevErrors, name: 'Name is required' }));
-        e.preventDefault();
-        return;
-      }
-  
-      // Validate Email
-      if (!validateEmail(formData.email)) {
-        setErrors((prevErrors) => ({
-          ...prevErrors,
-          email: 'Invalid email format',
-        }));
-        e.preventDefault();
-        return;
-      }
-  
-      // Validate Message
-      if (!formData.subject.trim()) {
-        setErrors((prevErrors) => ({
-          ...prevErrors,
-          subject: 'subject is required',
-        }));
-        e.preventDefault();
-        return;
-      }
-      // Validate Message
-      if (!formData.message.trim()) {
-        setErrors((prevErrors) => ({
-          ...prevErrors,
-          message: 'Message is required',
-        }));
-        e.preventDefault();
-        return;
-      }
-    };
-    
+  const handleSubmit = (e) => {
+    // Reset errors
+    setErrors({ name: '', email: '', message: '' });
+
+    // Validate Name
+    if (!formData.name.trim()) {
+      setErrors((prevErrors) => ({ ...prevErrors, name: 'Name is required' }));
+      e.preventDefault();
+      return;
+    }
+
+    // Validate Email
+    if (!validateEmail(formData.email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: 'Invalid email format',
+      }));
+      e.preventDefault();
+      return;
+    }
+
+    // Validate Message
+    if (!formData.subject.trim()) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        subject: 'subject is required',
+      }));
+      e.preventDefault();
+      return;
+    }
+    // Validate Message
+    if (!formData.message.trim()) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        message: 'Message is required',
+      }));
+      e.preventDefault();
+      return;
+    }
+  };
+
   return (
     <>
       <Navbar />
-      <div className='contact-us'>
-        <div className='contact-form'>
+      <div className="contact-us">
+        <div className="contact-form">
           <form
-          action={`mailto:mahmmmoud144@gmail.com?subject=Contact Form Submission&body=Name: ${formData.name}%0DEmail: ${formData.email}%0D%0DMessage: ${formData.message}`}
-          method="post"
-          onSubmit={handleSubmit}
-          />
-
-          <p className='c-p'>{typingText}</p>
-          <img className='c-img' src={img2}/>
-
-          <input 
-            className='c-input1' 
-            placeholder='Ad-Soyad'
-            value={formData.name}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          />
-          {errors.name && <div>{errors.name}</div>}
-          <input 
-            className='c-input2' 
-            placeholder='E-mail'
-            value={formData.email}
-            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          />
-          {errors.email && <div>{errors.email}</div>}
-          <input 
-            className='c-input3' 
-            placeholder='Konu Başlığı'
-            value={formData.subject}
-            onChange={(e) => setFormData({ ...formData, subject: e.target.value })}
-          />
-          {errors.subject && <div>{errors.subject}</div>}
-          <textarea 
-            className='c-text' 
-            placeholder='Mesaj'
-            value={formData.message}
-            onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+            action={`mailto:mahmmmoud144@gmail.com?subject=Contact Form Submission&body=Name: ${formData.name}%0DEmail: ${formData.email}%0D%0DMessage: ${formData.message}`}
+            method="post"
+            onSubmit={handleSubmit}
           >
-          </textarea>
-          {errors.message && <div>{errors.message}</div>}
-          <input type="submit" value="Send" className='c-button'/>
-        <form/>
-        </div>
-        <div className='contact-img'>
-          <img className='c-img1' src={img}/>
-          
-        </div>
+            <p className="c-p">{typingText}</p>
+            {/* <img className="c-img" src={img2} /> */}
 
+            <input
+              className="c-input1"
+              placeholder="Ad-Soyad"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+            />
+            {errors.name && <div>{errors.name}</div>}
+            <input
+              className="c-input2"
+              placeholder="E-mail"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+            />
+            {errors.email && <div>{errors.email}</div>}
+            <input
+              className="c-input3"
+              placeholder="Konu Başlığı"
+              value={formData.subject}
+              onChange={(e) =>
+                setFormData({ ...formData, subject: e.target.value })
+              }
+            />
+            {errors.subject && <div>{errors.subject}</div>}
+            <textarea
+              className="c-text"
+              placeholder="Mesaj"
+              value={formData.message}
+              onChange={(e) =>
+                setFormData({ ...formData, message: e.target.value })
+              }
+            ></textarea>
+            {errors.message && <div>{errors.message}</div>}
+            <button type="submit" className="c-button">
+              Send
+            </button>
+          </form>
+        </div>
+        <div className="contact-img">
+          <img className="c-img1" src={img} alt="contact_img" />
+        </div>
       </div>
 
       <Footer />
-  
     </>
   );
 };
